e8a8be521a3cca2828e7df2f1f9ff90d04d0c55e
pelgraine <140762863+pelgraine@users.noreply.github.com>
2026-03-21 18:39:06 +1100

    update firmware version and build date

--- examples/companion_radio/MyMesh.h
+++ examples/companion_radio/MyMesh.h
@@ -8,11 +8,11 @@
 #define FIRMWARE_VER_CODE 10
 
 #ifndef FIRMWARE_BUILD_DATE
-#define FIRMWARE_BUILD_DATE "20 March 2026"
+#define FIRMWARE_BUILD_DATE "21 March 2026"
 #endif
 
 #ifndef FIRMWARE_VERSION
-#define FIRMWARE_VERSION "Meck v1.2"
+#define FIRMWARE_VERSION "Meck v1.3"
 #endif
 
 #if defined(NRF52_PLATFORM) || defined(STM32_PLATFORM)
