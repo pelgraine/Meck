902577ed1098ebdabfd950441d4343433f1a9cef
pelgraine <140762863+pelgraine@users.noreply.github.com>
2026-03-28 01:11:26 +1100

    update build date

--- examples/companion_radio/MyMesh.h
+++ examples/companion_radio/MyMesh.h
@@ -8,7 +8,7 @@
 #define FIRMWARE_VER_CODE 10
 
 #ifndef FIRMWARE_BUILD_DATE
-#define FIRMWARE_BUILD_DATE "27 March 2026"
+#define FIRMWARE_BUILD_DATE "28 March 2026"
 #endif
 
 #ifndef FIRMWARE_VERSION
